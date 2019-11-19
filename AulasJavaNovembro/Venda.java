56b609a1b1ff499d74c3b97eb7abf9583b38566c
RENAN PONICK <renan.ponick@alunos.sc.senac.br>
2019-11-18 23:08:54 -0200

    Novembro

---
+++ AulasJavaNovembro/Venda.java
@@ -0,0 +1,23 @@
+package AulasJavaNovembro;
+
+import java.sql.Date;
+import java.util.Map;
+
+class Venda {
+    private int id;
+    private Date dataVenda;
+    private Funcionario funcionario;
+    private Cliente cliente;
+    private VendaProduto vendaProduto;
+    // ------------- Construtor --------------//
+        Venda(int id, Date dataVenda, Funcionario funcionario, Cliente cliente, VendaProduto vendaProduto){
+
+        }
+    // ------------- Fim Construtor --------------//
+    // ----------------- GET SET -----------------//
+
+    // -------------- FIM GET SET -----------------//
+    // ------------------ FUNÇÕES ---------------- //
+
+    // ---------------- FIM FUNÇÕES ------------- //
+}
